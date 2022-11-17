669869a82c70d4dc4c06a7ee64020c08b2796819
Liebe <pj8841466@gmail.com>
2022-11-17 13:37:42 +0530

    keyboard and text area is complete

--- src/Components/HangManWord.tsx
+++ src/Components/HangManWord.tsx
@@ -1,3 +1,33 @@
 export function HangManWord() {
-  return <div>hii</div>;
+  const word = "test";
+  const guessLetters = ["e", "t", "s"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span
+          key={index}
+          style={{
+            borderBottom: ".1em solid black",
+          }}
+        >
+          <span
+            style={{
+              visibility: guessLetters.includes(letter) ? "visible" : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
--- src/Components/KeyBoard.tsx
+++ src/Components/KeyBoard.tsx
@@ -1,3 +1,65 @@
+import { useEffect } from "react";
+
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 export function KeyBoard() {
-  return <div>hii</div>;
+  useEffect(() => {
+    const keyDownHandler = (event: { key: String }) => {
+      console.log("key", event.key);
+    };
+    document.addEventListener("keydown", keyDownHandler);
+  });
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "2rem",
+        flexWrap: "wrap",
+        fontSize: "2rem",
+        justifyContent: "center",
+        maxWidth: "100vw",
+      }}
+    >
+      {alphabet.map((letter, index) => (
+        <span
+          key={index}
+          style={{
+            background: "grey",
+            padding: "1rem 2rem",
+          }}
+          onClick={(e) => {
+            console.log((e.target as HTMLElement).textContent);
+          }}
+        >
+          {letter}
+        </span>
+      ))}
+    </div>
+  );
 }
